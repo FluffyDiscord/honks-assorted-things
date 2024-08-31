4edd7922cf362eb586ff9a8c19ea8eb1c4f87cf8
FluffyDiscord <rostislavkaleta@gmail.com>
2024-08-31 22:11:42 +0200

    add stacked variants

--- data.lua
+++ data.lua
@@ -3,3 +3,6 @@
 require("prototypes.item")
 require("prototypes.recipe")
 require("prototypes.technology")
+
+deadlock.add_stack("carbon-composite-frame", "__honks-assorted-things__/graphics/icons/stacked-material-textile-mesh-aramid.png", "carbon-composite-frames", 64, "item", 4)
+deadlock.add_stack("high-density-alloy", "__honks-assorted-things__/graphics/icons/stacked-part-heavy-cladding.png", "advanced-low-density-structures", 64, "item", 4)
